20218500daf3ae8e6c68e16f2e0a5f6e2255d967
Le Van Phuc <levanphuc24@gmail.com>
2019-12-30 15:08:15 +0700

    chapter3: Mostly static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:local] || I18n.default_locale
+  end
+
+  def default_url_options
+    {local: I18n.locale}
+  end
 end
