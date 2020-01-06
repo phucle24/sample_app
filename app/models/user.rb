5f7a402e48b310d1f4a96e9b924c328710d933d7
Le Van Phuc <levanphuc24@gmail.com>
2020-01-06 09:27:29 +0700

    Chapter_5 : Filling in the layout

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save{self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: Settings.users.name.size}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    length: {maximum: Settings.users.email.size},
+                      format: {with: VALID_EMAIL_REGEX},
+                        uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+                    length: {minimum: Settings.users.password.size}
+  has_secure_password
+end
