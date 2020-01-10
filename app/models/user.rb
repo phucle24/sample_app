358855355ec3ffc67ca5bbe93edb7608d635b552
Le Van Phuc <levanphuc24@gmail.com>
2020-01-10 07:56:49 +0700

    Chapter_9 : Advanced Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  before_save :mail
+  attr_accessor :remember_token
+  before_save :downcase_mail
   validates :name, presence: true,
                       length: {maximum: Settings.name_maximum}
   validates :email, presence: true,
@@ -9,18 +10,37 @@ class User < ApplicationRecord
   validates :password, presence: true,
                         length: {minimum: Settings.minimum_pass}
 
-  private
-
-  def mail
-    email.downcase!
-  end
-
   class << self
     def digest string
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  private
+
+  def downcase_mail
+    email.downcase!
   end
   has_secure_password
 end
